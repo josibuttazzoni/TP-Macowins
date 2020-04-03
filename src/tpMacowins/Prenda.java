ab35beb9cf13bcea7b1237c3da04dad5a45ec0e4
Josi Buttazzoni <mariajosebuttazzoni@hotmail.com>
2020-04-03 11:59:15 -0300

    Agrego tipo de prenda

--- src/tpMacowins/Prenda.java
+++ src/tpMacowins/Prenda.java
@@ -2,12 +2,18 @@ package tpMacowins;
 public class Prenda {
 	double precioBase;
 	Estado estado;
+	Tipo tipo;
 		
 	public double precio () {
 		return estado.getPrecio(precioBase);
 	}
 }
 
+enum Tipo {
+	Camisa,
+	Pantalon,
+	Saco
+}
 
 
 
